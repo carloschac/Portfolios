d1db936b63d6059857fc42516b72c723c1d07b5f
Carlos Henrique <carloss.chac@gmail.com>
2022-05-27 16:29:12 -0300

    Commit

--- nlwReturn/script.js
+++ nlwReturn/script.js
@@ -1,1 +1,14 @@
-alert('estou programando')
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+function closeMenu() {
+  document.body.classList.add('menu-expanded')
+}

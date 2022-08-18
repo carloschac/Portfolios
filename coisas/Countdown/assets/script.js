9cbb2aed375831b15bf6a43d087df4e53c07a23d
Carlos Henrique <carloss.chac@gmail.com>
2022-08-18 14:58:03 -0300

    Update script.js

--- coisas/Countdown/assets/script.js
+++ coisas/Countdown/assets/script.js
@@ -1,17 +1,30 @@
 
-    let countdown = new Date ("Aug 26, 2023 15;37:25").getTime();
+    let countdown = new Date ("Aug 28, 2022 14:56:25").getTime();
     let count = setInterval(function() {
         let getHour = new Date().getTime();
+        console.log(getHour)
         let distance = countdown - getHour;
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        console.log(distance)
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));   
         let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        document.querySelector("#dias").innerHTML = days
-        document.querySelector("#horas").innerHTML = hours
-        document.querySelector("#minutos").innerHTML = minutes
-        document.querySelector("#segundos").innerHTML = seconds
+        console.log(days)
+        console.log(hours)
+        console.log(minutes)
+        console.log(seconds)
+
+        document.querySelector("#dias").innerHTML = "0" + days;
+        document.querySelector("#horas").innerHTML = "0" + hours;
+        document.querySelector("#minutos").innerHTML = "0" + minutes;
+        document.querySelector("#segundos").innerHTML = seconds;
+
+        if (distance < 0) {
+            return;
+        }
+
+    
     }, 1000 )
 
 

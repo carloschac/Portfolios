bd3136d2bf81defd33fcc9f165e6ca6b6aad35d6
Carlos Henrique <carloss.chac@gmail.com>
2022-08-19 11:09:20 -0300

    Update script.js

--- coisas/Countdown/assets/script.js
+++ coisas/Countdown/assets/script.js
@@ -16,11 +16,31 @@
         console.log(seconds)
 
         document.querySelector("#dias").innerHTML = days;
+        if (days < 10) {
+            document.querySelector("#dias").innerHTML = "0" + days;
+        }
+
         document.querySelector("#horas").innerHTML = hours;
+        if (hours < 10) {
+            document.querySelector("#horas").innerHTML = "0" + hours;
+        }
+
         document.querySelector("#minutos").innerHTML = minutes;
+        if (minutes < 10) {
+            ocument.querySelector("#minutos").innerHTML = minutes;
+        }
+
         document.querySelector("#segundos").innerHTML = seconds;
+        if (seconds <10) {
+            document.querySelector("#segundos").innerHTML = "0" + seconds;
+        }
     
     }, 1000 )
+
+    function resetCountdown() {
+        clearInterval(distance);
+        
+    }
 
     
 
